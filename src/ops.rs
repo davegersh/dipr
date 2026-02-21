96559cb27eb0130c8b36af7645e69869c1fd6de1
davegersh <davegershman465@gmail.com>
2026-02-20 22:50:29 -0500

    Implemented custom macro for generating element-wise operations.

--- src/ops.rs
+++ src/ops.rs
@@ -1,195 +1,89 @@
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 use super::tensor::Tensor;
 
-//
-// ------ Addition ------
-//
-impl AddAssign<&Tensor> for Tensor {
-    fn add_assign(&mut self, rhs: &Tensor) {
-        for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
-            *a += b
+// Handles implementation of any element wise operation (including scalar)
+macro_rules! element_op {
+    ($assign_trait:ident, $assign_func:ident, $assign_op:tt, $op_trait:ident, $op_func:ident, $op:tt) => {
+        // Assignment
+        impl $assign_trait<&Tensor> for Tensor {
+            fn $assign_func(&mut self, rhs: &Tensor) {
+                for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+                    *a $assign_op b
+                }
+            }
         }
-    }
-}
 
-// Ref + Ref
-impl Add<&Tensor> for &Tensor {
-    type Output = Tensor;
-    fn add(self, other: &Tensor) -> Self::Output {
-        let mut op_clone = self.clone();
-        op_clone += other;
-        op_clone
-    }
-}
-
-// Owned + Ref
-impl Add<&Tensor> for Tensor {
-    type Output = Tensor;
-    fn add(mut self, other: &Tensor) -> Self::Output {
-        self += other;
-        self
-    }
-}
-
-// Ref + Owned
-impl Add<Tensor> for &Tensor {
-    type Output = Tensor;
-    fn add(self, mut other: Tensor) -> Self::Output {
-        other += self;
-        other
-    }
-}
-
-// Owned + Owned
-impl Add<Tensor> for Tensor {
-    type Output = Self;
-    fn add(self, other: Tensor) -> Self::Output {
-        self + &other
-    }
-}
-
-//
-// ------ Subtraction ------
-//
-impl SubAssign<&Tensor> for Tensor {
-    fn sub_assign(&mut self, rhs: &Tensor) {
-        for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
-            *a -= b
+        // Ref + Ref
+        impl $op_trait<&Tensor> for &Tensor {
+            type Output = Tensor;
+            fn $op_func(self, other: &Tensor) -> Self::Output {
+                let mut op_clone = self.clone();
+                op_clone $assign_op other;
+                op_clone
+            }
         }
-    }
-}
 
-// Ref - Ref
-impl Sub<&Tensor> for &Tensor {
-    type Output = Tensor;
-    fn sub(self, other: &Tensor) -> Self::Output {
-        let mut op_clone = self.clone();
-        op_clone -= other;
-        op_clone
-    }
-}
-
-// Owned - Ref
-impl Sub<&Tensor> for Tensor {
-    type Output = Tensor;
-    fn sub(mut self, other: &Tensor) -> Self::Output {
-        self -= other;
-        self
-    }
-}
-
-// Ref - Owned
-impl Sub<Tensor> for &Tensor {
-    type Output = Tensor;
-    fn sub(self, mut other: Tensor) -> Self::Output {
-        other -= self;
-        other
-    }
-}
-
-// Owned - Owned
-impl Sub<Tensor> for Tensor {
-    type Output = Self;
-    fn sub(self, other: Tensor) -> Self::Output {
-        self - &other
-    }
-}
-
-
-//
-// ------ Multiplication ------
-//
-impl MulAssign<&Tensor> for Tensor {
-    fn mul_assign(&mut self, rhs: &Tensor) {
-        for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
-            *a *= b
+        // Owned + Ref
+        impl $op_trait<&Tensor> for Tensor {
+            type Output = Tensor;
+            fn $op_func(mut self, other: &Tensor) -> Self::Output {
+                self $assign_op other;
+                self
+            }
         }
-    }
-}
 
-// Ref * Ref
-impl Mul<&Tensor> for &Tensor {
-    type Output = Tensor;
-    fn mul(self, other: &Tensor) -> Self::Output {
-        let mut op_clone = self.clone();
-        op_clone *= other;
-        op_clone
-    }
-}
-
-// Owned * Ref
-impl Mul<&Tensor> for Tensor {
-    type Output = Tensor;
-    fn mul(mut self, other: &Tensor) -> Self::Output {
-        self *= other;
-        self
-    }
-}
-
-// Ref * Owned
-impl Mul<Tensor> for &Tensor {
-    type Output = Tensor;
-    fn mul(self, mut other: Tensor) -> Self::Output {
-        other *= self;
-        other
-    }
-}
-
-// Owned * Owned
-impl Mul<Tensor> for Tensor {
-    type Output = Self;
-    fn mul(self, other: Tensor) -> Self::Output {
-        self * &other
-    }
-}
-
-
-//
-// ------ Division ------
-//
-impl DivAssign<&Tensor> for Tensor {
-    fn div_assign(&mut self, rhs: &Tensor) {
-        for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
-            *a /= b
+        // Ref + Owned
+        impl $op_trait<Tensor> for &Tensor {
+            type Output = Tensor;
+            fn $op_func(self, mut other: Tensor) -> Self::Output {
+                other $assign_op self;
+                other
+            }
         }
-    }
+
+        // Owned + Owned
+        impl $op_trait<Tensor> for Tensor {
+            type Output = Tensor;
+            fn $op_func(self, other: Tensor) -> Self::Output {
+                self $op &other
+            }
+        }
+
+        // += f32
+        impl $assign_trait<&f32> for Tensor {
+            fn $assign_func(&mut self, rhs: &f32) {
+                for a in self.data.iter_mut() {
+                    *a $assign_op rhs
+                }
+            }
+        }
+
+        // Owned + f32
+        impl $op_trait<&f32> for Tensor {
+            type Output = Tensor;
+            fn $op_func(mut self, rhs: &f32) -> Self::Output {
+                self $assign_op rhs;
+                self
+            }
+        }
+
+        // Owned + f32
+        impl $op_trait<&f32> for &Tensor {
+            type Output = Tensor;
+            fn $op_func(self, rhs: &f32) -> Self::Output {
+                self.clone() $op rhs
+            }
+        }
+    };
 }
 
-// Ref / Ref
-impl Div<&Tensor> for &Tensor {
-    type Output = Tensor;
-    fn div(self, other: &Tensor) -> Self::Output {
-        let mut op_clone = self.clone();
-        op_clone /= other;
-        op_clone
-    }
-}
-
-// Owned / Ref
-impl Div<&Tensor> for Tensor {
-    type Output = Tensor;
-    fn div(mut self, other: &Tensor) -> Self::Output {
-        self /= other;
-        self
-    }
-}
-
-// Ref / Owned
-impl Div<Tensor> for &Tensor {
-    type Output = Tensor;
-    fn div(self, mut other: Tensor) -> Self::Output {
-        other /= self;
-        other
-    }
-}
-
-// Owned / Owned
-impl Div<Tensor> for Tensor {
-    type Output = Self;
-    fn div(self, other: Tensor) -> Self::Output {
-        self / &other
-    }
-}
+//
+// ---- Addition, Subtraction, Multiplication, Division -----
+//
+element_op!(AddAssign, add_assign, +=, Add, add, +);
+element_op!(SubAssign, sub_assign, -=, Sub, sub, -);
+element_op!(MulAssign, mul_assign, *=, Mul, mul, *);
+element_op!(DivAssign, div_assign, /=, Div, div, /);
 
 //
 // ---- Negation ----
